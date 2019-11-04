f5b4e0749c9d0b8ea0d81aae790e719cd3fb2913
dimgeorgiev1976 <dim.georgiev1976@gmail.com>
2019-11-04 13:43:32 +0000

    new anchor && set && get

--- engine/Sprite.js
+++ engine/Sprite.js
@@ -4,21 +4,68 @@
 
     class Sprite {
         // Загруженое с клиентами изображение в аргументе
-        constructor (textura) {
+        constructor (textura , args = {}) {
             this.textura = textura
+
+            const frame = args.frame || {}
+
             // Кусочек изображение каторой нужни отрисовать
             this.frame = {
-                x: 0,
-                y: 0,
-                width: textura.width,
-                height: textura.height
+                x: frame.x || 0,
+                y: frame.y || 0,
+                width: frame.width || textura.width,
+                height: frame.height || textura.height
             }
             // Oтрисовка sprite
-            this.x = 0
-            this.y = 0
-            this.width = this.frame.width
-            this.height = this.frame.height
+            this.x = args.x || 0 ,
+            this.y = args.y || 0 ,
+            // Kakая част от изображение, нужно взят в % что бъй появлилас
+            //  настаящий център изображение
+            this.anchorX = args.x || 0 ,
+            this.anchorY = args.y || 0 ,
+            this.width = args.width || this.frame.width
+            this.height = args.height || this.frame.height
+
+            if ( args.scale !== undefined  ) {
+                this.setScale(args.scale)
+            }
         }
+
+        setScale (value) {
+            this.scaleX = value
+            this.scaleY = value
+        }
+
+        get absoluteX () {
+            // въйчислит где настаящий Х
+            return this.x - this.anchorX * this.width
+        }
+
+        // въйчислит где настаящий Y
+        get absoluteY () {
+            return this.y - this.anchorY * this.height
+        }
+
+        // Функция для запрашевание и получение
+        get scaleX () {
+            // Въйзъйваться функция и возврашаться отншение
+            return this.width / this.frame.width 
+        }
+        // Функция для задание значение sprite на лету
+        set scaleX (value) {
+            this.width = this.frame.width * value
+            return value
+        }
+
+        get scaleY () {
+            return this.height / this.frame.height
+        }
+
+        set scaleY (value) {
+            this.height = this.frame.height * value
+            return value
+        }
+
         // Meтод для Oтрисовка sprite
         draw ( canvas, context ) {
             context.drawImage(
@@ -31,17 +78,14 @@
                 this.frame.height ,
 
                 // Координатъй участък где нужно отабразит на canvase
-                this.x ,
-                this.y ,
+                // верхние, левъй угъл
+                this.absoluteX ,
+                this.absoluteY ,
                 this.width ,
                 this.height 
             )
         }
     }
-
-
-
-
 
 
     window.GameEngine = window.GameEngine || {}
